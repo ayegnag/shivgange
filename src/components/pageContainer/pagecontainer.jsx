5e26b55fc249d91a1ad777a9c5803e84abbc023c
ayegnag <gangeya.u@gmail.com>
2021-06-29 01:25:20 +0530

    Adjusted caard

--- src/components/pageContainer/pagecontainer.jsx
+++ src/components/pageContainer/pagecontainer.jsx
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from "react";
 import CountDown from "../countdownTimer/countdown";
 import SplitTitle from "../splitTitle/splittitle";
-import InviteCard from "../inviteCard/invite"
+import InviteCard from "../inviteCard/invite";
 import "./pagecontainer.css";
 
 export default function PageContainer() {
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [celebrate, setCelebrate] = useState(false);
+
+  const isToday = (date) => {
+    const today = new Date();
+    console.log(today, date);
+    return (
+      date.getDate() === today.getDate() &&
+      date.getMonth() === today.getMonth() &&
+      date.getFullYear() === today.getFullYear()
+    );
+  };
 
   const handleScroll = () => {
     const percentScroll = Math.floor(
@@ -16,6 +27,8 @@ export default function PageContainer() {
   };
 
   useEffect(() => {
+    const eventDate = new Date(2021, 6, 1);
+    setCelebrate(isToday(eventDate));
     window.addEventListener("scroll", handleScroll, { passive: true });
     return () => {
       window.removeEventListener("scroll", handleScroll);
@@ -25,8 +38,11 @@ export default function PageContainer() {
   return (
     <div className="pageContainer">
       <SplitTitle scrollVal={scrollPosition}></SplitTitle>
-      <InviteCard></InviteCard>
-      {/* <CountDown scrollVal={scrollPosition}></CountDown>  */}
+      {celebrate ? (
+        <InviteCard></InviteCard>
+      ) : (
+        <CountDown scrollVal={scrollPosition}></CountDown>
+      )}
       <div className={scrollPosition > 5 ? "backdrop blurr" : "backdrop"}></div>
     </div>
   );
